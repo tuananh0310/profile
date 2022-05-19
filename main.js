1c4846c98f5b88204aa825e742c8db06a8decf8f
tuananh0310 <tuananh0310@gmail.com>
2022-05-19 14:52:08 +0700

    ok

---
+++ main.js
@@ -0,0 +1,5 @@
+const clickImg = document.querySelector('img');
+function change() {
+    clickImg.setAttribute('src', 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/279495107_3144516059121487_8415778391474294421_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=7FTgKBLsTOgAX-yP6k5&_nc_ht=scontent.fhan2-1.fna&oh=00_AT--C5DMZe79WdjnWKGdzKULssSJOmdv2HG6MYMIE4hSDA&oe=628BBD58')
+}
+
